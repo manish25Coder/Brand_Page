ba45c7cb1e0755f9966b58a61f637be4a1c409be
manish25Coder <manishsingh59766@gmail.com>
2023-10-14 13:01:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Content from './component/Content'
+import Navigator from './component/Navigator'
+
+function App() {
+
+
+  return (
+    <>
+    <Navigator/>
+    <Content/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Navigator.jsx
@@ -0,0 +1,20 @@
+function Navigator() {
+    return(
+        <>
+         <nav>
+        <div className='logo'></div>
+        <img src='/images/brand_logo.png' alt="Brand Logo"/>
+        <ul>
+            <li href="#">MENU</li>
+            <li href="#">LOCATION</li>
+            <li href="#">ABOUT</li>
+            <li href="#">CONTACT</li>
+
+        </ul>
+        <button>Login</button>
+        </nav>
+        </>
+    );
+    
+}
+export default Navigator;
